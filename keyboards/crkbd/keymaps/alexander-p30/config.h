935cc95c7eabb0e0a6a389e0cf4a13613e786399
alexander-p30 <alexanderp30e@gmail.com>
2022-11-17 20:54:21 -0300

    tweaks

--- keyboards/crkbd/keymaps/alexander-p30/config.h
+++ keyboards/crkbd/keymaps/alexander-p30/config.h
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
-
 /* Select hand configuration */
 
 #define MASTER_LEFT
@@ -29,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 150
+#define TAPPING_TERM 180
 #define PERMISSIVE_HOLD
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
@@ -42,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
-    /* #define RGBLIGHT_EFFECT_BREATHING */
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_BREATHING
+    /* #define RGBLIGHT_EFFECT_RAINBOW_MOOD */
     /* #define RGBLIGHT_EFFECT_RAINBOW_SWIRL */
     /* #define RGBLIGHT_EFFECT_SNAKE */
     /* #define RGBLIGHT_EFFECT_KNIGHT */
@@ -52,10 +50,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     /* #define RGBLIGHT_EFFECT_RGB_TEST */
     /* #define RGBLIGHT_EFFECT_ALTERNATING */
     /* #define RGBLIGHT_EFFECT_TWINKLE */
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 50
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT	
+    #define RGBLIGHT_DEFAULT_HUE 180
+    #define RGBLIGHT_SLEEP
 #endif
+
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
