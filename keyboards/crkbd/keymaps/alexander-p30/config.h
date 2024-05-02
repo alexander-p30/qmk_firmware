da45ce7b1d8f96939ed506403b126b7fa8981b68
alexander-p30 <alexanderp30e@gmail.com>
2024-05-02 11:40:15 -0300

    Fixes after qmk update

--- keyboards/crkbd/keymaps/alexander-p30/config.h
+++ keyboards/crkbd/keymaps/alexander-p30/config.h
@@ -30,9 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 130
 #define PERMISSIVE_HOLD
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // Turn on caps word by pressing lsft and rsft simultaneously
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 #define CAPS_WORD_IDLE_TIMEOUT 1500
@@ -41,22 +38,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
-    // #define RGBLIGHT_EFFECT_BREATHING
-    // #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    // #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    // #define RGBLIGHT_EFFECT_SNAKE
-    // #define RGBLIGHT_EFFECT_KNIGHT
-    // #define RGBLIGHT_EFFECT_CHRISTMAS
-    // #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    // #define RGBLIGHT_EFFECT_RGB_TEST
-    // #define RGBLIGHT_EFFECT_ALTERNATING
-    // #define RGBLIGHT_EFFECT_TWINKLE
-    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
-    #define RGBLIGHT_DEFAULT_HUE 178
-    #define RGBLIGHT_DEFAULT_VAL 20
-    #define RGBLIGHT_DEFAULT_SAT 170
-    #define RGBLIGHT_SLEEP
+// #define RGBLIGHT_EFFECT_BREATHING
+// #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #define RGBLIGHT_EFFECT_SNAKE
+// #define RGBLIGHT_EFFECT_KNIGHT
+// #define RGBLIGHT_EFFECT_CHRISTMAS
+// #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #define RGBLIGHT_EFFECT_RGB_TEST
+// #define RGBLIGHT_EFFECT_ALTERNATING
+// #define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+#    define RGBLIGHT_DEFAULT_HUE 178
+#    define RGBLIGHT_DEFAULT_VAL 20
+#    define RGBLIGHT_DEFAULT_SAT 170
+#    define RGBLIGHT_SLEEP
 #endif
-
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
