8d94b5c5dd70933779913809e1cee9aab3964dfc
alexander-p30 <alexanderp30e@gmail.com>
2024-05-02 11:09:20 -0300

    Update

--- keyboards/crkbd/keymaps/alexander-p30/config.h
+++ keyboards/crkbd/keymaps/alexander-p30/config.h
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 150
+#define TAPPING_TERM 130
 #define PERMISSIVE_HOLD
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
@@ -41,20 +41,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 
 #ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    /* #define RGBLIGHT_EFFECT_RAINBOW_MOOD */
-    /* #define RGBLIGHT_EFFECT_RAINBOW_SWIRL */
-    /* #define RGBLIGHT_EFFECT_SNAKE */
-    /* #define RGBLIGHT_EFFECT_KNIGHT */
-    /* #define RGBLIGHT_EFFECT_CHRISTMAS */
-    /* #define RGBLIGHT_EFFECT_STATIC_GRADIENT */
-    /* #define RGBLIGHT_EFFECT_RGB_TEST */
-    /* #define RGBLIGHT_EFFECT_ALTERNATING */
-    /* #define RGBLIGHT_EFFECT_TWINKLE */
-    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT	
+    // #define RGBLIGHT_EFFECT_BREATHING
+    // #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    // #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    // #define RGBLIGHT_EFFECT_SNAKE
+    // #define RGBLIGHT_EFFECT_KNIGHT
+    // #define RGBLIGHT_EFFECT_CHRISTMAS
+    // #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    // #define RGBLIGHT_EFFECT_RGB_TEST
+    // #define RGBLIGHT_EFFECT_ALTERNATING
+    // #define RGBLIGHT_EFFECT_TWINKLE
+    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
     #define RGBLIGHT_DEFAULT_HUE 178
-    #define RGBLIGHT_DEFAULT_VAL 60
-    #define RGBLIGHT_DEFAULT_SAT 240
+    #define RGBLIGHT_DEFAULT_VAL 20
+    #define RGBLIGHT_DEFAULT_SAT 170
     #define RGBLIGHT_SLEEP
 #endif
 
